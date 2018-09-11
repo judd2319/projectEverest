0352a7b6b3dfb8b89dd5c555587bac941b265cf8
Kai Schneider <36481203+Kaisch7@users.noreply.github.com>
2018-09-11 19:23:11 -0400

    JuddTest

--- src/Everest/BasicLand.java
+++ src/Everest/BasicLand.java
@@ -8,6 +8,8 @@ package Everest;
  * 
  *         This class represents the most basic land square in the game, it is
  *         the brown grassland that supplies one source of food per turn.
+ *         
+ *         WenisMeister
  */
 public class BasicLand {
 
